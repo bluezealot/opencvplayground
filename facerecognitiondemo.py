a67bbfddedb4bfbe3645a8876c20e425e44debb7
bluezealot <qin_xizhou@hoperun.com>
2023-03-26 11:54:54 +0800

    Improve process speed and multi-face recognition.

--- facerecognitiondemo.py
+++ facerecognitiondemo.py
@@ -1,12 +1,15 @@
 from os import listdir
 import face_recognition
 import cv2
-import time
+import numpy as np
 
 
 class FaceRecognitionDemo:
     known_face_encodings = []
     known_face_names = []
+    process_frame = True
+    face_locations = []
+    face_names = []
 
     def load_samples(self, path):
         for file in listdir(path):
@@ -16,39 +19,45 @@ class FaceRecognitionDemo:
                 self.known_face_names.append(file.rsplit('.')[0])
 
     def recognize(self, target_image):
-        image_encoding = face_recognition.face_encodings(target_image)
-        if len(image_encoding) > 0:
-            results = face_recognition.compare_faces(self.known_face_encodings, image_encoding[0])
+        locations = face_recognition.face_locations(target_image)
+        image_encodings = face_recognition.face_encodings(target_image, locations)
+        names = []
+        for image_encoding in image_encodings:
+            results = face_recognition.compare_faces(self.known_face_encodings, image_encoding)
+            face_distances = face_recognition.face_distance(self.known_face_encodings, image_encoding)
+            best_match = np.argmin(face_distances)
             name = 'Unknown'
-            if not True in results:
-                print('Unknown face')
-            else:
-                name = self.known_face_names[results.index(True)]
-            return name, face_recognition.face_locations(target_image)[0]
-        return '', [0, 0, 0, 0]
+            if results[best_match]:
+                name = self.known_face_names[best_match]
+            names.append(name)
+        return names, locations
+
+    def process_frame_recognize(self, target_frame):
+        if self.process_frame:
+            small_frame = cv2.resize(target_frame, (0, 0), fx=0.25, fy=0.25)
+            small_rgb = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+            self.face_names, self.face_locations = self.recognize(small_rgb)
+        self.process_frame = not self.process_frame
+        for face_name, (y1, x1, y2, x2) in zip(self.face_names, self.face_locations):
+            y1 *= 4
+            x1 *= 4
+            y2 *= 4
+            x2 *= 4
+            cv2.putText(target_frame, face_name, (x2, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 200, 0), 2)
+            cv2.rectangle(target_frame, (x1, y1), (x2, y2), (0, 200, 0), 2)
+        return target_frame
 
 
 if __name__ == "__main__":
-    # start camera
-    cap = cv2.VideoCapture(0)
     face_rec = FaceRecognitionDemo()
     face_rec.load_samples('face_samples')
-    rec_times = 0
-    y1, x1, y2, x2 = 0, 0, 0, 0
-    face_name = ''
+    # start camera
+    cap = cv2.VideoCapture(0)
     while True:
         ret, frame = cap.read()
         if ret:
-            frame = cv2.resize(frame, (0, 0), fx=0.3, fy=0.3)
-            if rec_times % 5 == 0:
-                face_name, face_shape = face_rec.recognize(frame)
-                y1, x1, y2, x2 = face_shape[0], face_shape[1], face_shape[2], face_shape[3]
-                rec_times = 0
-            rec_times = rec_times + 1
-            cv2.putText(frame, face_name, (x2, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 200))
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 2)
-            cv2.imshow('video', frame)
-            time.sleep(0.01)
+            processed_frame = face_rec.process_frame_recognize(frame)
+            cv2.imshow('recognized', processed_frame)
         else:
             break
         if cv2.waitKey(10) == ord('q'):
